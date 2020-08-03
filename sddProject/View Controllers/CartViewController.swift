cd20758503a93e31108e83c8cb4bbec4e295ca1c
MysticalX <44600936+MysticalX@users.noreply.github.com>
2020-08-03 15:13:02 +0800

    added cart

---
+++ sddProject/View Controllers/CartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CartViewController.swift
+//  sddProject
+//
+//  Created by ITP312Grp2 on 3/8/20.
+//  Copyright © 2020 ITP312Grp2. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
